7a04ad9a00f388f8ed5872eed97f2a71126b7b2d
Mohamed Shaman <it19029900@my.sliit.lk>
2022-12-16 00:00:33 +0530

    06 - 009 Working with Route Parameters To Pass Data Between Screens

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,7 +6,10 @@ import { CATEGORIES } from "../data/dummy-data";
 function CategoriesScreen({ navigation }) {
   function renderCategoryItem(itemData) {
     function pressHandler() {
-      navigation.navigate("MealsOverview");
+      //passing route with "params"
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
     }
 
     return (
