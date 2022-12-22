e2ba32eb3ee256d8521aa1bd0bbe940d15ed9df0
Mohamed Shaman <it19029900@my.sliit.lk>
2022-12-23 00:40:54 +0530

    06 - 023 Final

--- App.js
+++ App.js
@@ -3,12 +3,36 @@ import { StyleSheet, Button } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+        }}
+      />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -25,10 +49,13 @@ export default function App() {
         >
           <Stack.Screen
             name="MealsCategories"
-            component={CategoriesScreen}
+            //home page එක සමග DrawerNavigator function එක link කරයි
+            component={DrawerNavigator}
             //Style header of single route page
             options={{
-              title: "All Categories",
+              // -- hide the header--
+              headerShown: false,
+              // title: "All Categories",
               // headerStyle: { backgroundColor: "#351401" },
               // headerTintColor: "white",
               // contentStyle: { backgroundColor: "#3f2f25" },
